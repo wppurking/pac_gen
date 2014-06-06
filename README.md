d5dea1345e799ee69f2e331cccebd702bdf14a8d
wyatt <wppurking@gmail.com>
2014-06-06 14:31:23 +0800

    support mac os pac file format

--- README.md
+++ README.md
@@ -9,5 +9,5 @@
 此脚本将会根据 xxx_domain/ xxx_domain_tiny 中记录的域名生成符合 [Proxy Auto-config](http://www.proxypacfiles.com/proxypac/static/netscape-proxy-format.html) 规则的 PAC File 文件, 并且使用 shExpMatch 进行域名的模糊匹配以及使用 SOCKS5 Direct 代替 SOCKS 避免使用 SOCK4 协议.
 
 
-## pac_file.pac
-这个文件为最后一次使用 pac.rb 脚本生成的目标 pac 文件
+## pac_file.pac, pac_file_socks5.pac
+这个文件为最后一次使用 pac.rb 脚本生成的目标 pac 文件.  有 socsk5 文件是因为在正常的 [Proxy Auto-config](http://www.proxypacfiles.com/proxypac/static/netscape-proxy-format.html) 文件中, *Return Value Format* 中是只有 DIRECT, PROXY, SOCKS 这三种形式的, 但是在 Chrome 浏览器中使用 SOCKS 无效, 使用 SOCKS5 执行才有效, 而在 Mac OS 系统的 PAC 配置, 只有当使用 SOCKS 有效, 使用 SOCKS5 无效. 所以会独立生成两份文件, 以便给 Mac OS 使用.
--- pac.rb
+++ pac.rb
@@ -1,7 +1,7 @@
 # 根据 proxy_body 生成输出最后的 pac_file 内容
-def pac_file(proxy_body)
+def pac_file(proxy_body, socks5: true)
   %Q(function FindProxyForURL(url, host) {
-  var PROXY = "SOCKS5 127.0.0.1:8181";
+  var PROXY = "SOCKS#{socks5 ? '5' : ''} 127.0.0.1:8181";
   var DEFAULT = "DIRECT";
 
 #{proxy_body}
@@ -24,6 +24,7 @@ file_paths = ['google_domain_tiny', 'amazon_domain_tiny', 'ebay_domain_tiny', 'f
 
 
 proxy_body = file_paths.map { |path| domain_to_proxy_body(path) }.join("")
-File.open('./pac_file.pac', 'w') { |io| io.write(pac_file(proxy_body)) }
+File.open('./pac_file_socks5.pac', 'w') { |io| io.write(pac_file(proxy_body)) }
+File.open('./pac_file.pac', 'w') { |io| io.write(pac_file(proxy_body, socks5: false)) }
 
 
